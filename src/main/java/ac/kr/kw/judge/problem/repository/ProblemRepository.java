42d598e21e88b562635f06c2174f7e7b33d938ea
bugdebugging <annyousung@gmail.com>
2022-02-20 14:39:34 +0900

    feat:TestCase 패치조인 쿼리

--- src/main/java/ac/kr/kw/judge/problem/repository/ProblemRepository.java
+++ src/main/java/ac/kr/kw/judge/problem/repository/ProblemRepository.java
@@ -2,6 +2,12 @@ package ac.kr.kw.judge.problem.repository;
 
 import ac.kr.kw.judge.problem.domain.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+    @Query("SELECT P FROM Problem P LEFT JOIN FETCH P.testCases WHERE  P.id = :problemId")
+    Optional<Problem> findByIdWithTestCases(@Param("problemId") Long problemId);
 }
