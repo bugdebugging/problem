92e61400983a5a9f686d9658ae29e406b90c2ae2
bugdebugging <annyousung@gmail.com>
2022-03-02 13:05:23 +0900

    fix:ApiResult로 api 응답 통일

--- src/main/java/ac/kr/kw/judge/problem/adapter/in/web/ProblemRegisterController.java
+++ src/main/java/ac/kr/kw/judge/problem/adapter/in/web/ProblemRegisterController.java
@@ -1,6 +1,10 @@
 package ac.kr.kw.judge.problem.adapter.in.web;
 
+import ac.kr.kw.judge.commons.apis.ApiResult;
+import ac.kr.kw.judge.commons.apis.ApiUtils;
 import ac.kr.kw.judge.problem.adapter.out.execute.exception.FileHashFailedException;
+import ac.kr.kw.judge.problem.domain.Description;
+import ac.kr.kw.judge.problem.domain.Limit;
 import ac.kr.kw.judge.problem.domain.TestCase;
 import ac.kr.kw.judge.problem.dto.ProblemRegisterRequest;
 import ac.kr.kw.judge.problem.service.command.ProblemRegisterCommand;
@@ -8,8 +12,10 @@ import ac.kr.kw.judge.problem.service.port.in.ProblemRegisterService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +27,8 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 @Api(tags = {"problems"})
 @RestController
 @RequiredArgsConstructor
@@ -29,10 +37,14 @@ public class ProblemRegisterController {
 
     @ApiOperation(value = "새로운 문제 등록", notes = "테스트 케이스와 함께 새로운 문제 등록.")
     @PostMapping(value = "/api/problems")
-    public Long registerProblem(
-            @RequestPart(value = "problem") ProblemRegisterRequest problemRegisterRequest,
+    public ApiResult<Long> registerProblem(
+            String name, String description,
+            @RequestParam("input_description") String inputDescription,
+            @RequestParam("output_description") String outputDescription,
+            int time, int memory, int score,
             @RequestPart(value = "inputs") List<MultipartFile> inputFiles,
             @RequestPart(value = "outputs") List<MultipartFile> outputFiles) {
+        checkArgument(inputFiles.size() == outputFiles.size(), "업로드할 input과 output파일의 수는 동일해야합니다.");
 
         File rootDir = new File("//problems//" + UUID.randomUUID().toString());
         rootDir.mkdirs();
@@ -49,12 +61,10 @@ public class ProblemRegisterController {
                     (i + 1) + ""));
         }
 
-        ProblemRegisterCommand problemRegisterCommand = new ProblemRegisterCommand(problemRegisterRequest.getName(),
-                problemRegisterRequest.getDescription(),
-                problemRegisterRequest.getLimit(),
-                testcases,
-                problemRegisterRequest.getScore());
-        return problemRegisterService.registerNewProblem(problemRegisterCommand);
+        ProblemRegisterCommand problemRegisterCommand = new ProblemRegisterCommand(name
+                , Description.of(description, inputDescription, outputDescription)
+                , Limit.of(memory, time), testcases, score);
+        return ApiUtils.success(problemRegisterService.registerNewProblem(problemRegisterCommand));
     }
 
     private List<String> saveFilesToLocal(File rootFile, List<MultipartFile> files) {
--- src/main/java/ac/kr/kw/judge/problem/adapter/in/web/SolutionGradingController.java
+++ src/main/java/ac/kr/kw/judge/problem/adapter/in/web/SolutionGradingController.java
@@ -1,5 +1,7 @@
 package ac.kr.kw.judge.problem.adapter.in.web;
 
+import ac.kr.kw.judge.commons.apis.ApiResult;
+import ac.kr.kw.judge.commons.apis.ApiUtils;
 import ac.kr.kw.judge.problem.dto.GradingResult;
 import ac.kr.kw.judge.problem.dto.Submit;
 import ac.kr.kw.judge.problem.service.port.in.SolutionGradingService;
@@ -19,7 +21,7 @@ public class SolutionGradingController {
 
     @ApiOperation(value = "소스코드 제출", notes = "문제에 대한 솔루션 코드 제출")
     @PostMapping("/api/problems/{problemId}/grading")
-    public GradingResult gradingSolution(@PathVariable("problemId")Long problemId, @RequestBody Submit submit){
-        return solutionGradingService.gradeSolution(problemId,submit);
+    public ApiResult<GradingResult> gradingSolution(@PathVariable("problemId") Long problemId, @RequestBody Submit submit) {
+        return ApiUtils.success(solutionGradingService.gradeSolution(problemId, submit));
     }
 }
