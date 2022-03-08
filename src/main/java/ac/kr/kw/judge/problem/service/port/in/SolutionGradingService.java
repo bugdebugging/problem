0bcc0c50a7503f012ee0544ccca7acd17ee6a2f9
bugdebugging <annyousung@gmail.com>
2022-03-08 14:55:07 +0900

    fix:challenge 채점용과 일반 채점용으로 인터페이스 나누기

--- src/main/java/ac/kr/kw/judge/problem/service/port/in/SolutionGradingService.java
+++ src/main/java/ac/kr/kw/judge/problem/service/port/in/SolutionGradingService.java
@@ -1,8 +1,11 @@
 package ac.kr.kw.judge.problem.service.port.in;
 
-import ac.kr.kw.judge.problem.dto.GradingResult;
+import ac.kr.kw.judge.problem.domain.event.ChallengeSubmitted;
+import ac.kr.kw.judge.problem.domain.event.GradingResult;
 import ac.kr.kw.judge.problem.dto.Submit;
 
 public interface SolutionGradingService {
     GradingResult gradeSolution(Long problemId, Submit submit);
+
+    void gradeChallengeSubmit(Long problemId, ChallengeSubmitted challengeSubmitted);
 }
